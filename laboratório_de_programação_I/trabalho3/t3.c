ec9697ec03df388ed812f6cf4d64800c02697500
Jean WSL <jeanalemao2006@gmail.com>
2026-05-31 10:15:57 -0300

    Exibindo multiplas notas, no entando o cursor ainda está fixo e as funções de movimento não funcionam corretamente

--- laboratório_de_programação_I/trabalho3/t3.c
+++ laboratório_de_programação_I/trabalho3/t3.c
@@ -1,254 +1,381 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include "tela.h"
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <stdbool.h>
+  #include "tela.h"
 
-//JEAN
-typedef struct {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-} cor;
+  //JEAN
+  typedef struct {
+      unsigned char r;
+      unsigned char g;
+      unsigned char b;
+  } cor;
 
-typedef struct {
-    int x;
-    int y;
-    int largura;
-    int altura;
-} retangulo;
+  typedef struct {
+      int x;
+      int y;
+      int largura;
+      int altura;
+  } retangulo;
 
-typedef struct {
-    char texto[150];
-    cor cor;
-    retangulo retangulo;
-    char etiqueta[4];
-} nota;
+  typedef struct {
+      char texto[150];
+      cor cor;
+      retangulo retangulo;
+      char etiqueta[4];
+  } nota;
+
+  typedef enum { move, edita, fim } modo_t;
+
+  typedef struct {
+    modo_t modo;
+
+    nota *notas;
+    int n_notas;
+
+    int cursor_x;
+    int cursor_y;
+
+  } estado_t;
 
 
-// desenha um retângulo colorido com texto dentro
-void desenha_ret(int l, int c, int dl, int dc,
-                 int r, int g, int b,
-                 char s[])
-{
-  // desenha o retângulo com espaços, selecionando a cor como fundo
-  t_corfundo(r, g, b);
-  // imprime dl linhas com dc espaços cada
-  for (int i = 0; i < dl; i++) {
-    // posiciona o cursor onde inicia essa linha do retângulo
-    t_lincol(l + i, c);
-    // %10s imprime pelo menos 10 caracteres, completando com espaços
-    //   se a string for menor
-    // %.10s imprime no máximo 10 caracteres
-    // %10.10s imprime exatamente 10 caracteres
-    // se um número for substituído por '*', será pego no próximo argumento
-    printf("%*s", dc, ""); // imprime dc espaços (podia tbém ser um for)
+  // desenha um retângulo colorido com texto dentro
+  void desenha_ret(int l, int c, int dl, int dc,
+                  int r, int g, int b,
+                  char s[])
+  {
+    // desenha o retângulo com espaços, selecionando a cor como fundo
+    t_corfundo(r, g, b);
+    // imprime dl linhas com dc espaços cada
+    for (int i = 0; i < dl; i++) {
+      // posiciona o cursor onde inicia essa linha do retângulo
+      t_lincol(l + i, c);
+      // %10s imprime pelo menos 10 caracteres, completando com espaços
+      //   se a string for menor
+      // %.10s imprime no máximo 10 caracteres
+      // %10.10s imprime exatamente 10 caracteres
+      // se um número for substituído por '*', será pego no próximo argumento
+      printf("%*s", dc, ""); // imprime dc espaços (podia tbém ser um for)
+    }
+
+    // escreve o tamanho da tela no canto do retângulo
+    int alt, larg;
+    t_tamanho(&alt, &larg);
+    t_lincol(l, c);
+    printf("%dx%d", alt, larg);
+
+    // escreve s no centro do retângulo
+    t_lincol(l + dl/2, c + dc/2 - strlen(s)/2);
+    t_cortexto(0, 0, 0);
+    printf("%s", s);
   }
 
-  // escreve o tamanho da tela no canto do retângulo
-  int alt, larg;
-  t_tamanho(&alt, &larg);
-  t_lincol(l, c);
-  printf("%dx%d", alt, larg);
-
-  // escreve s no centro do retângulo
-  t_lincol(l + dl/2, c + dc/2 - strlen(s)/2);
-  t_cortexto(0, 0, 0);
-  printf("%s", s);
-}
-
-typedef enum { move, edita, fim } modo_t;
-
-typedef struct {
-  modo_t modo;
-  nota nota;
-} estado_t;
-
-void muda_modo(estado_t *e, modo_t novo_modo)
-{
-  e->modo = novo_modo;
-}
-
-// modo "move"
-
-void tela_move(estado_t *e)
-{
-  t_limpa();
-  desenha_ret(2, 3, 20, 50, 10, 20, 30, "fundo");
-  desenha_ret(e->nota.retangulo.y, e->nota.retangulo.x, e->nota.retangulo.altura, e->nota.retangulo.largura, e->nota.cor.r, e->nota.cor.g, e->nota.cor.b, e->nota.texto);
-  // põe o cursor no centro do retângulo
-  t_lincol(e->nota.retangulo.y + e->nota.retangulo.altura/2, e->nota.retangulo.x + e->nota.retangulo.largura/2);
-}
-
-void move_direita(estado_t *e)
-{
-  e->nota.retangulo.x++;
-}
-
-void move_esquerda(estado_t *e)
-{
-  e->nota.retangulo.x--;
-}
-
-void exec_move(estado_t *e)
-{
-  // modo sem necessidade de manter valores locais, implementado sem
-  //   laço próprio
-
-  // desenha a tela
-  tela_move(e);
-
-  // lê um comando
-  tecla_t tec = t_tecla();
-
-  // realiza uma ação conforme o comando lido
-  switch (tec) {
-    case T_ESQUERDA:
-      move_esquerda(e);
-      break;
-    case T_DIREITA:
-      move_direita(e);
-      break;
-    case 'e':
-      muda_modo(e, edita);
-      break;
-    case T_ESC:
-      muda_modo(e, fim);
-      break;
-    default: ;
+  void muda_modo(estado_t *e, modo_t novo_modo)
+  {
+    e->modo = novo_modo;
   }
-}
 
-// modo edita
+  // modo "move"
 
-void tela_edita(estado_t *e, char texto[])
-{
-  t_limpa();
-  desenha_ret(2, 3, 20, 50, 10, 20, 30, "fundo");
-  desenha_ret(e->nota.retangulo.y, e->nota.retangulo.x, e->nota.retangulo.altura, e->nota.retangulo.largura, 150, 49, 97, texto);
-}
-
-void remove_ultimo_caractere(char s[])
-{
-  int l = strlen(s);
-  if (l > 0) {
-    s[l - 1] = '\0';
+  void tela_move(estado_t *e)
+  {
+    t_limpa();
+    desenha_ret(1, 1, 30, 70, 10, 20, 30, "fundo");
+    for (int i = 0; i < e->n_notas; i++) {
+      desenha_ret(e->notas[i].retangulo.y, e->notas[i].retangulo.x, e->notas[i].retangulo.altura, e->notas[i].retangulo.largura,
+                  e->notas[i].cor.r, e->notas[i].cor.g, e->notas[i].cor.b, e->notas[i].texto);
+    }
+    // põe o cursor no centro do retângulo
+    t_lincol(e->notas[0].retangulo.y + e->notas[0].retangulo.altura/2, e->notas[0].retangulo.x + e->notas[0].retangulo.largura/2);
   }
-}
 
-void insere_ultimo_caractere(char s[], char c)
-{
-  int l = strlen(s);
-  s[l] = c;
-  s[l + 1] = '\0';
-}
+  void move_direita(estado_t *e)
+  {
+    e->cursor_x++;
+  }
 
-void exec_edita(estado_t *e)
-{
-  // modo com necessidade de manter valores locais, implementado com
-  //   laço próprio
-  char txt[20] = "";
+  void move_esquerda(estado_t *e)
+  {
+    e->cursor_x--;
+  }
 
-  while (e->modo == edita) {
+  void move_cima(estado_t *e)
+  {
+    e->cursor_y--;
+  }
+
+  void move_baixo(estado_t *e)
+  {
+    e->cursor_y++;
+  }
+
+  void move_nota_direita(estado_t *e)
+  {
+    e->notas[0].retangulo.x++;
+  }
+
+  void move_nota_esquerda(estado_t *e)
+  {
+    e->notas[0].retangulo.x--;
+  }
+
+  void move_nota_cima(estado_t *e)
+  {
+    e->notas[0].retangulo.y--;
+  }
+
+  void move_nota_baixo(estado_t *e)
+  {
+    e->notas[0].retangulo.y++;
+  }
+
+  void diminui_direita(estado_t *e)
+  {
+    e->notas[0].retangulo.largura--;
+  }
+
+  void diminui_esquerda(estado_t *e)
+  {
+    e->notas[0].retangulo.x++;
+    e->notas[0].retangulo.largura--;
+  }
+
+  void diminui_cima(estado_t *e)
+  {
+    e->notas[0].retangulo.y++;
+    e->notas[0].retangulo.altura--;
+  }
+
+  void diminui_baixo(estado_t *e)
+  {
+    e->notas[0].retangulo.altura--;
+  }
+
+  void aumenta_direita(estado_t *e)
+  {
+    e->notas[0].retangulo.largura++;
+  }
+
+  void aumenta_esquerda(estado_t *e)
+  {
+    e->notas[0].retangulo.x--;
+    e->notas[0].retangulo.largura++;
+  }
+
+  void aumenta_cima(estado_t *e)
+  {
+    e->notas[0].retangulo.altura++;
+  }
+
+  void aumenta_baixo(estado_t *e)
+  {
+    e->notas[0].retangulo.y--;
+    e->notas[0].retangulo.altura++;
+  }
+
+  void exec_move(estado_t *e)
+  {
+    // modo sem necessidade de manter valores locais, implementado sem
+    //   laço próprio
+
     // desenha a tela
-    tela_edita(e, txt);
+    //PAREI AQUI
+    tela_move(e);
 
     // lê um comando
     tecla_t tec = t_tecla();
 
     // realiza uma ação conforme o comando lido
-    if (tec == T_ESC) {
-      muda_modo(e, move);
-    } else if (tec == T_BS) {
-      remove_ultimo_caractere(txt);
-    } else if (tec >= 'a' && tec <= 'z') {
-      insere_ultimo_caractere(txt, tec);
+    switch (tec) {
+      case T_ESQUERDA:
+        move_esquerda(e);
+        break;
+      case T_DIREITA:
+        move_direita(e);
+        break;
+      case T_CIMA:
+        move_cima(e);
+        break;
+      case T_BAIXO:
+        move_nota_baixo(e);
+        break;
+      case T_SHIFT_ESQUERDA:
+        move_nota_esquerda(e);
+        break;
+      case T_SHIFT_DIREITA:
+        move_nota_direita(e);
+        break;
+      case T_SHIFT_CIMA:
+        move_nota_cima(e);
+        break;
+      case T_SHIFT_BAIXO:
+        move_nota_baixo(e);
+        break;
+      case T_ALT_ESQUERDA:
+        diminui_esquerda(e);
+        break;
+      case T_ALT_DIREITA:
+        diminui_direita(e);
+        break;
+      case T_ALT_CIMA:
+        diminui_cima(e);
+        break;
+      case T_ALT_BAIXO:
+        diminui_baixo(e);
+        break;
+      case T_CTRL_ESQUERDA:
+        aumenta_esquerda(e);
+        break;
+      case T_CTRL_DIREITA:
+        aumenta_direita(e);
+        break;
+      case T_CTRL_CIMA:
+        aumenta_cima(e);
+        break;
+      case T_CTRL_BAIXO:
+        aumenta_baixo(e);
+        break;
+      case 'e':
+        muda_modo(e, edita);
+        break;
+      case T_ESC:
+        muda_modo(e, fim);
+        break;
+      default: ;
     }
   }
-}
 
-//JEAN
-int ler_notas (FILE *arq, int *n_notas, nota **notas) {
-    int cont, c;
-    while (fscanf(arq, "%s %hhu %hhu %hhu %d %d %d %d ", 
-        (*notas)[(*n_notas) - 1].etiqueta,
-        &(*notas)[(*n_notas) - 1].cor.r,
-        &(*notas)[(*n_notas) - 1].cor.g,
-        &(*notas)[(*n_notas) - 1].cor.b,
-        &(*notas)[(*n_notas) - 1].retangulo.x,
-        &(*notas)[(*n_notas) - 1].retangulo.y,
-        &(*notas)[(*n_notas) - 1].retangulo.largura,
-        &(*notas)[(*n_notas) - 1].retangulo.altura
-    ) == 8) {
-        cont = 0;
+  // modo edita
 
-        //Serve para ignorar a primeira " da palavra
-        fgetc(arq);
+  void tela_edita(estado_t *e, char texto[])
+  {
+    t_limpa();
+    desenha_ret(2, 3, 20, 50, 10, 20, 30, "fundo");
+    strcat(e->notas[0].texto, texto);
+    desenha_ret(e->notas[0].retangulo.y, e->notas[0].retangulo.x, e->notas[0].retangulo.altura, e->notas[0].retangulo.largura, 150, 49, 97, e->notas[0].texto);
+  }
 
-
-        //AINDA PRECISA RESOLVER QUANDO UMA LINHA TEM MULTIPLAS ""
-        while ((c = fgetc(arq)) != '"')
-        {
-            (*notas)[(*n_notas) - 1].texto[cont] = c;
-            cont++;
-        }
-        (*notas)[(*n_notas) - 1].texto[cont] = '\0';
-        
-        (*n_notas)++;
-        (*notas) = realloc((*notas), (*n_notas) * sizeof(nota));
-        if ((*notas) == NULL) {
-            printf("Não foi possível realocar a memória");
-            return -1;
-        }
+  void remove_ultimo_caractere(char s[])
+  {
+    int l = strlen(s);
+    if (l > 0) {
+      s[l - 1] = '\0';
     }
+  }
 
-    (*n_notas)--;
-    (*notas) = realloc((*notas), (*n_notas) * sizeof(nota));
-    if ((*notas) == NULL) {
-        printf("Não foi possível realocar a memória");
-        return -1;
+  void insere_ultimo_caractere(char s[], char c)
+  {
+    int l = strlen(s);
+    s[l] = c;
+    s[l + 1] = '\0';
+  }
+
+  void exec_edita(estado_t *e)
+  {
+    // modo com necessidade de manter valores locais, implementado com
+    //   laço próprio
+    char txt[20] = "";
+
+    while (e->modo == edita) {
+      // desenha a tela
+      tela_edita(e, txt);
+
+      // lê um comando
+      tecla_t tec = t_tecla();
+
+      // realiza uma ação conforme o comando lido
+      if (tec == T_ESC) {
+        muda_modo(e, move);
+      } else if (tec == T_BS) {
+        remove_ultimo_caractere(txt);
+      } else if (tec >= 'a' && tec <= 'z') {
+        insere_ultimo_caractere(txt, tec);
+      }
     }
-    return 0;
-}
+  }
+
+  //JEAN
+  int ler_notas (FILE *arq, int *n_notas, nota **notas) {
+      int cont, c;
+      while (fscanf(arq, "%s %hhu %hhu %hhu %d %d %d %d ", 
+          (*notas)[(*n_notas) - 1].etiqueta,
+          &(*notas)[(*n_notas) - 1].cor.r,
+          &(*notas)[(*n_notas) - 1].cor.g,
+          &(*notas)[(*n_notas) - 1].cor.b,
+          &(*notas)[(*n_notas) - 1].retangulo.x,
+          &(*notas)[(*n_notas) - 1].retangulo.y,
+          &(*notas)[(*n_notas) - 1].retangulo.largura,
+          &(*notas)[(*n_notas) - 1].retangulo.altura
+      ) == 8) {
+          cont = 0;
+
+          //Serve para ignorar a primeira " da palavra
+          fgetc(arq);
 
 
-int main (void) {
+          //AINDA PRECISA RESOLVER QUANDO UMA LINHA TEM MULTIPLAS ""
+          while ((c = fgetc(arq)) != '"')
+          {
+              (*notas)[(*n_notas) - 1].texto[cont] = c;
+              cont++;
+          }
+          (*notas)[(*n_notas) - 1].texto[cont] = '\0';
+          
+          (*n_notas)++;
+          (*notas) = realloc((*notas), (*n_notas) * sizeof(nota));
+          if ((*notas) == NULL) {
+              printf("Não foi possível realocar a memória");
+              return -1;
+          }
+      }
 
-    // Le o arquivo
-    int n_notas = 1;
-    int *ptr_n_notas = &n_notas;
-    nota *notas = malloc(n_notas * sizeof(nota));
-    char file_name[10];
-    printf("Digite o arquivo para ler as notas: \n");
-    scanf("%s", file_name);
-    FILE *arq = fopen(file_name, "r");
-    if (arq == NULL) {
-        printf("Não consegui abrir o arquivo!\n");
-        return -1;
-    }
-
-    int conseguiu_ler = ler_notas(arq, ptr_n_notas, &notas);
-    if (conseguiu_ler == -1) {
-        return -1;
-    }
+      (*n_notas)--;
+      (*notas) = realloc((*notas), (*n_notas) * sizeof(nota));
+      if ((*notas) == NULL) {
+          printf("Não foi possível realocar a memória");
+          return -1;
+      }
+      return 0;
+  }
 
 
-    t_inicia();
-    estado_t estado = { move, notas[0] };
-    while (estado.modo != fim) {
-        switch (estado.modo) {
-        case move:
-            exec_move(&estado);
-            break;
-        case edita:
-            exec_edita(&estado);
-            break;
-        default:
-            break;
-        }
-        
-    }
-    // retorna a tela ao modo normal
-    t_fim();
-    fclose(arq);
-}
+  int main (void) {
+
+      // Le o arquivo
+      int n_notas = 1;
+      int *ptr_n_notas = &n_notas;
+      nota *notas = malloc(n_notas * sizeof(nota));
+      char file_name[10];
+      printf("Digite o arquivo para ler as notas: \n");
+      scanf("%s", file_name);
+      FILE *arq = fopen(file_name, "r");
+      if (arq == NULL) {
+          printf("Não consegui abrir o arquivo!\n");
+          return -1;
+      }
+
+      int conseguiu_ler = ler_notas(arq, ptr_n_notas, &notas);
+      if (conseguiu_ler == -1) {
+          return -1;
+      }
+
+
+      t_inicia();
+      estado_t estado = { move, notas, n_notas, 5, 5 };
+      while (estado.modo != fim) {
+          switch (estado.modo) {
+          case move:
+              exec_move(&estado);
+              break;
+          case edita:
+              exec_edita(&estado);
+              break;
+          default:
+              break;
+          }
+          
+      }
+      // retorna a tela ao modo normal
+      t_fim();
+      fclose(arq);
+  }

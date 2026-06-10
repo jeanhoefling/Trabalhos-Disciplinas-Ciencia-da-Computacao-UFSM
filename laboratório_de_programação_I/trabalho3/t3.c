1f5ebf58f80305c30f708608681ff6f7720636e6
jean WSL PC <jeanalemao2006@gmail.com>
2026-06-10 18:37:26 -0300

    Inserindo a possibilidade de apagar todas as notas e melhorando as validações da função ler notas

--- laboratório_de_programação_I/trabalho3/t3.c
+++ laboratório_de_programação_I/trabalho3/t3.c
@@ -48,114 +48,31 @@ typedef struct {
   unsigned char cor_edicao[3];
 } estado_t;
 
+// FUNÇÕES DE UTILIDADE GERAL
+
 int realoca_memoria(estado_t *e, int dmemoria) {
-  if (e->n_notas + dmemoria == 0) {
-    printf("Você não deve excluir todas as notas!");
-    return 0;
+
+  // Caso especifico em que se deseja deletar a última nota
+  if (e->n_notas == 1 && dmemoria == -1) {
+    free(e->notas);
+    e->notas = NULL;
+    e->n_notas = 0;
+    return 1;
   }
+
+  // Caso geral
   nota *tmp = e->notas;
   tmp = realloc(tmp, (e->n_notas + dmemoria) * sizeof(nota));
+
   if (tmp == NULL) {
     printf("Não foi possível realocar memória.\n");
     return 0;
   }
+
   e->notas = tmp;
   e->n_notas += dmemoria;
   return 1;
 }
-
-void atualiza_nota_corrente (estado_t *e) {
-  retangulo atual;
-  e->nota_corrente = NULL;
-  for (int i = 0; i < e->n_notas; i++) {
-    atual = e->notas[i].retangulo;
-    if (e->cursor_x >= atual.x && e->cursor_x < (atual.x + atual.largura) && e->cursor_y >= atual.y && e->cursor_y < atual.y + atual.altura) {
-      e->nota_corrente = &(e->notas[i]);
-    }
-  }
-}
-
-void mover_inicio (estado_t *e) {
-  if (e->nota_corrente == NULL) {
-    return;
-  }
-  int indice = e->nota_corrente - e->notas;
-  nota tmp = *(e->nota_corrente);
-  for (int i = indice; i > 0; i--) {
-    e->notas[i] = e->notas[i - 1];
-  }
-  e->notas[0] = tmp;
-  atualiza_nota_corrente(e);
-}
-
-void mover_fim (estado_t *e) {
-  if (e->nota_corrente == NULL) {
-    return;
-  }
-  int indice = e->nota_corrente - e->notas;
-  nota tmp = *(e->nota_corrente);
-  for (int i = indice; i < e->n_notas - 1; i++) {
-    e->notas[i] = e->notas[i + 1];
-  }
-  e->notas[e->n_notas - 1] = tmp;
-  atualiza_nota_corrente(e);
-}
-
-void gravar_notas (estado_t *e, char file_name[]) {
-  FILE *arq = fopen(file_name, "w");
-  if (arq == NULL) {
-    printf("Não consegui abrir o arquivo para gravar\n");
-    return;
-  }
-  for (int i = 0; i < e->n_notas; i++) {
-    fprintf(arq, "%s %hhu %hhu %hhu %d %d %d %d \"%s\"\n", 
-            e->notas[i].etiqueta, e->notas[i].cor.r, e->notas[i].cor.g, e->notas[i].cor.b, e->notas[i].retangulo.x, e->notas[i].retangulo.y,
-          e->notas[i].retangulo.largura, e->notas[i].retangulo.altura, e->notas[i].texto);
-  }
-  fclose(arq);
-}
-
-void nova_nota (estado_t *e) {
-  if(!realoca_memoria(e, 1)) return;
-
-
-  nota *n = &(e->notas[e->n_notas - 1]);
-  strcpy(n->texto, "Nova nota");
-  strcpy(n->etiqueta, "A00");
-  n->cor.r = 120;
-  n->cor.g = 120;
-  n->cor.b = 120;
-  n->retangulo.x = e->cursor_x;
-  n->retangulo.y = e->cursor_y;
-  n->retangulo.largura = 5;
-  n->retangulo.altura = 5;
-}
-
-void deleta_nota (estado_t *e) {
-  int indice;
-  if (e->nota_corrente != NULL) {
-    e->nota_removida = *(e->nota_corrente);
-    indice = e->nota_corrente - e->notas;
-    for(int i = indice; i < e->n_notas - 1; i++) {
-      e->notas[i] = e->notas[i + 1];
-    }
-    if(!realoca_memoria(e, -1)) return;
-    e->existe_nota_removida = 1;
-  }
-  atualiza_nota_corrente(e);
-}
-
-void inserir_nota_removida (estado_t *e) {
-  if (e->existe_nota_removida) {
-    if(!realoca_memoria(e, 1)) return;
-    e->nota_removida.retangulo.x = e->cursor_x;
-    e->nota_removida.retangulo.y = e->cursor_y;
-    e->notas[e->n_notas - 1] = e->nota_removida;
-    e->existe_nota_removida = 0;
-    atualiza_nota_corrente(e);
-  }
-}
-
 
 // desenha um retângulo colorido com texto dentro
 void desenha_ret(int y, int x, int altura, int largura,
@@ -227,30 +144,110 @@ void muda_modo(estado_t *e, modo_t novo_modo)
   e->modo = novo_modo;
 }
 
-// modo "move"
-void tela_principal(estado_t *e)
-{
-  t_limpa();
-  desenha_ret(1, 1, e->altura_fundo, e->largura_fundo, 10, 20, 30, "fundo");
+
+// FUNÇÕES DO MODO PRINCIPAL
+
+void atualiza_nota_corrente (estado_t *e) {
+  retangulo atual;
+  e->nota_corrente = NULL;
   for (int i = 0; i < e->n_notas; i++) {
-    if (strcmp(e->texto_busca, "") == 0 || strstr(e->notas[i].texto, e->texto_busca)) {
-      desenha_ret(e->notas[i].retangulo.y, e->notas[i].retangulo.x, e->notas[i].retangulo.altura, e->notas[i].retangulo.largura,
-                  e->notas[i].cor.r, e->notas[i].cor.g, e->notas[i].cor.b, e->notas[i].texto);
-      }
-  }
-  if (e->nota_corrente != NULL) {
-    if ((e->nota_corrente->cor.r + e->nota_corrente->cor.g + e->nota_corrente->cor.b)/3 < 120) {
-      t_corfundo(255, 255, 255);
-      t_cortexto(0, 0, 0);
+    if (!(strstr(e->notas[i].texto, e->texto_busca))) {
+      continue;
     }
-    else {
-      t_corfundo(0, 0, 0);
-      t_cortexto(255, 255, 255);
+    atual = e->notas[i].retangulo;
+    if (e->cursor_x >= atual.x && e->cursor_x < (atual.x + atual.largura) && e->cursor_y >= atual.y && e->cursor_y < atual.y + atual.altura) {
+      e->nota_corrente = &(e->notas[i]);
     }
-    t_lincol(e->nota_corrente->retangulo.y, e->nota_corrente->retangulo.x + e->nota_corrente->retangulo.largura - 1);
-    printf("c");
   }
-  t_lincol(e->cursor_y, e->cursor_x);
+}
+
+void mover_inicio (estado_t *e) {
+  if (e->nota_corrente == NULL) {
+    return;
+  }
+  int indice = e->nota_corrente - e->notas;
+  nota tmp = *(e->nota_corrente);
+  for (int i = indice; i > 0; i--) {
+    e->notas[i] = e->notas[i - 1];
+  }
+  e->notas[0] = tmp;
+  atualiza_nota_corrente(e);
+}
+
+void mover_fim (estado_t *e) {
+  if (e->nota_corrente == NULL) {
+    return;
+  }
+  int indice = e->nota_corrente - e->notas;
+  nota tmp = *(e->nota_corrente);
+  for (int i = indice; i < e->n_notas - 1; i++) {
+    e->notas[i] = e->notas[i + 1];
+  }
+  e->notas[e->n_notas - 1] = tmp;
+  atualiza_nota_corrente(e);
+}
+
+void gravar_notas (estado_t *e, char file_name[]) {
+  FILE *arq = fopen(file_name, "w");
+  if (arq == NULL) {
+    printf("Não consegui abrir o arquivo para gravar\n");
+    return;
+  }
+  for (int i = 0; i < e->n_notas; i++) {
+    fprintf(arq, "%s %hhu %hhu %hhu %d %d %d %d \"%s\"\n", 
+            e->notas[i].etiqueta, e->notas[i].cor.r, e->notas[i].cor.g, e->notas[i].cor.b, e->notas[i].retangulo.x, e->notas[i].retangulo.y,
+          e->notas[i].retangulo.largura, e->notas[i].retangulo.altura, e->notas[i].texto);
+  }
+  fclose(arq);
+}
+
+void nova_nota (estado_t *e) {
+  if(!realoca_memoria(e, 1)) return;
+  nota *n = &(e->notas[e->n_notas - 1]);
+  strcpy(n->texto, "Nova nota");
+  strcpy(n->etiqueta, "A00");
+  n->cor.r = 120;
+  n->cor.g = 120;
+  n->cor.b = 120;
+  n->retangulo.x = e->cursor_x;
+  n->retangulo.y = e->cursor_y;
+  n->retangulo.largura = 5;
+  n->retangulo.altura = 5;
+}
+
+void deleta_nota (estado_t *e) {
+  int indice;
+  if (e->nota_corrente != NULL && e->n_notas > 0) {
+    e->nota_removida = *(e->nota_corrente);
+    indice = e->nota_corrente - e->notas;
+    for(int i = indice; i < e->n_notas - 1; i++) {
+      e->notas[i] = e->notas[i + 1];
+    }
+    if(!realoca_memoria(e, -1)) return;
+    e->existe_nota_removida = 1;
+  }
+  atualiza_nota_corrente(e);
+}
+
+void inserir_nota_removida (estado_t *e) {
+  if (e->existe_nota_removida) {
+    if(!realoca_memoria(e, 1)) return;
+    e->nota_removida.retangulo.x = e->cursor_x;
+    e->nota_removida.retangulo.y = e->cursor_y;
+    e->notas[e->n_notas - 1] = e->nota_removida;
+    e->existe_nota_removida = 0;
+    atualiza_nota_corrente(e);
+  }
+}
+
+void posiciona_cursor_ultima_nota(estado_t *e) {
+  for (int i = e->n_notas - 1; i >= 0; i--) {
+    if(strstr(e->notas[i].texto, e->texto_busca)) {
+      e->cursor_x = e->notas[i].retangulo.x;
+      e->cursor_y = e->notas[i].retangulo.y;
+      return;
+    }
+  }
 }
 
 void move_direita(estado_t *e)
@@ -378,6 +375,30 @@ void aumenta_baixo(estado_t *e)
     }
 }
 
+void tela_principal(estado_t *e)
+{
+  t_limpa();
+  desenha_ret(1, 1, e->altura_fundo, e->largura_fundo, 10, 20, 30, "fundo");
+  for (int i = 0; i < e->n_notas; i++) {
+    if (strcmp(e->texto_busca, "") == 0 || strstr(e->notas[i].texto, e->texto_busca)) {
+      desenha_ret(e->notas[i].retangulo.y, e->notas[i].retangulo.x, e->notas[i].retangulo.altura, e->notas[i].retangulo.largura,
+                  e->notas[i].cor.r, e->notas[i].cor.g, e->notas[i].cor.b, e->notas[i].texto);
+      }
+  }
+  if (e->nota_corrente != NULL) {
+    if ((e->nota_corrente->cor.r + e->nota_corrente->cor.g + e->nota_corrente->cor.b)/3 < 120) {
+      t_corfundo(255, 255, 255);
+      t_cortexto(0, 0, 0);
+    }
+    else {
+      t_corfundo(0, 0, 0);
+      t_cortexto(255, 255, 255);
+    }
+    t_lincol(e->nota_corrente->retangulo.y, e->nota_corrente->retangulo.x + e->nota_corrente->retangulo.largura - 1);
+    printf("c");
+  }
+  t_lincol(e->cursor_y, e->cursor_x);
+}
 
 void exec_principal(estado_t *e, char file_name[])
 {
@@ -444,8 +465,7 @@ void exec_principal(estado_t *e, char file_name[])
       inserir_nota_removida(e);
       break;
     case 'p':
-      e->cursor_x = e->notas[e->n_notas - 1].retangulo.x;
-      e->cursor_y = e->notas[e->n_notas - 1].retangulo.y;
+      posiciona_cursor_ultima_nota(e);
       break;
     case 'i':
       mover_inicio(e);
@@ -477,16 +497,7 @@ void exec_principal(estado_t *e, char file_name[])
   }
 }
 
-// modo edita nota
-void tela_edita_texto(char texto[], int cur)
-{
-  t_limpa();
-  //pode ser melhorada para mostrar a nota corrente
-  t_lincol(1, 1);
-  printf("%s", texto);
-  t_lincol(1, cur + 1);
-}
-
+// FUNÇÕES MODO EDITA TEXTO
 void move_cursor_edita (int *cur, int dx, char texto[]) {
   if ((*cur) + dx >= 0 && (*cur) + dx <= strlen(texto)) {
     (*cur) += dx;
@@ -517,6 +528,14 @@ void grava_texto(estado_t *e, char txt[]) {
   strcpy(e->nota_corrente->texto, txt);
 }
 
+void tela_edita_texto(char texto[], int cur)
+{
+  t_limpa();
+  //pode ser melhorada para mostrar a nota corrente
+  t_lincol(1, 1);
+  printf("%s", texto);
+  t_lincol(1, cur + 1);
+}
 
 void exec_edita_texto(estado_t *e)
 {
@@ -564,7 +583,8 @@ void exec_edita_texto(estado_t *e)
   }
 }
 
-//modo edita_tbusca
+// FUNÇÕES EDITA TEXTO DE BUSCA
+
 void tela_edita_tbusca (char txt[], int cursor) {
   t_limpa();
   t_lincol(1, 1);
@@ -620,6 +640,17 @@ void exec_edita_tbusca(estado_t *e)
   }
 }
 
+// FUNÇÕES EDITA COR
+
+unsigned char soma_digito_cor (tecla_t tec, unsigned char c) {
+  if (c * 10 + (tec - '0') <= 255) {
+    return c * 10 + (tec - '0');
+  }
+  else {
+    return c;
+  }
+}
+
 void tela_edita_cor (estado_t *e, int c) {
   t_limpa();
   desenha_ret(1, 1, 5, 39, 0, 0, 0, "  r    g    b  ");
@@ -644,15 +675,6 @@ void tela_edita_cor (estado_t *e, int c) {
   t_corfundo(e->cor_edicao[0], e->cor_edicao[1], e->cor_edicao[2]);
   printf(" ");
   t_lincol(1, 9);
-}
-
-unsigned char soma_digito_cor (tecla_t tec, unsigned char c) {
-  if (c * 10 + (tec - '0') <= 255) {
-    return c * 10 + (tec - '0');
-  }
-  else {
-    return c;
-  }
 }
 
 void exec_edita_cor (estado_t *e) {
@@ -747,68 +769,91 @@ void exec_edita_cor (estado_t *e) {
 }
 
 int ler_notas (FILE *arq, FILE *erros, estado_t *e) {
-    int cont, c, tem_erro;
-    char str_erro[200];
-    while (fscanf(arq, "%s %hhu %hhu %hhu %d %d %d %d ", 
-        e->notas[e->n_notas - 1].etiqueta,
-        &e->notas[e->n_notas - 1].cor.r,
-        &e->notas[e->n_notas - 1].cor.g,
-        &e->notas[e->n_notas - 1].cor.b,
-        &e->notas[e->n_notas - 1].retangulo.x,
-        &e->notas[e->n_notas - 1].retangulo.y,
-        &e->notas[e->n_notas - 1].retangulo.largura,
-        &e->notas[e->n_notas - 1].retangulo.altura
-    ) == 8) {
-        cont = 0;
+  char etiqueta[50], texto[500], texto_cortado[150];
+  int r, g, b, x, y, largura, altura; 
+  int tem_erro, cont, c;
+  while (fscanf(arq, "%s %d %d %d %d %d %d %d ", etiqueta, &r, &g, &b, &x, &y, &largura, &altura) == 8) {
+    cont = 0;
+    tem_erro = 0;
 
-        //Serve para ignorar a primeira " da palavra
-        fgetc(arq);
+    // Serve pra ignorar a primeira " da palavra
+    fgetc(arq);
 
-        tem_erro = 0;
-        while ((c = fgetc(arq)) != '"'
-        && cont < 149)
-        {
-            if (c >= 32 && c <= 126) {
-              e->notas[e->n_notas - 1].texto[cont] = c;
-              cont++;
-            }
-            else {
-              tem_erro = 1;
-            }
-        }
-        e->notas[e->n_notas - 1].texto[cont] = '\0';
-
-        str_erro[0] = c;
-        cont = 1;
-
-        // Serve para ignorar texto sobrando "inválido"
-        while((c = fgetc(arq)) != '\n' && c != EOF) {
-          tem_erro = 1;
-          str_erro[cont] = c;
-          cont++;
-        };
-        str_erro[cont] = '\0';
-        if (tem_erro) {
-          fprintf(erros, "%s %hhu %hhu %hhu %d %d %d %d \"%s%s\n",
-                e->notas[e->n_notas - 1].etiqueta,
-                e->notas[e->n_notas - 1].cor.r,
-                e->notas[e->n_notas - 1].cor.g,
-                e->notas[e->n_notas - 1].cor.b,
-                e->notas[e->n_notas - 1].retangulo.x,
-                e->notas[e->n_notas - 1].retangulo.y,
-                e->notas[e->n_notas - 1].retangulo.largura,
-                e->notas[e->n_notas - 1].retangulo.altura,
-                e->notas[e->n_notas - 1].texto,
-                str_erro);
-        }
-        
-        if(!realoca_memoria(e, 1)) return -1;
+    while ((c = fgetc(arq)) != '\"') {
+      texto[cont] = c;
+      cont++;
     }
-    fprintf(erros, "\n");
-    if(!realoca_memoria(e, -1)) return -1;
-    return 0;
-}
 
+    while ((c = fgetc(arq)) != '\n' && c != EOF) {
+      texto[cont] = c;
+      cont++;
+      tem_erro = 1;
+    }
+
+    texto[cont] = '\0';
+
+    // Etiqueta maior que devia
+    if (strlen(etiqueta) != 3) {
+      tem_erro = 1;
+    }
+
+    // Etiqueta inválida
+    for (int i = 0; i < 3; i++) {
+      if (!((etiqueta[i] >= 'A' && etiqueta[i] <= 'Z') ||
+          (etiqueta[i] >= '0' && etiqueta[i] <= '9'))) {
+            tem_erro = 1;
+      }
+    }
+
+    if ((r < 0 || r > 255) || (g < 0 || g > 255) || (b < 0 || b > 255)) {
+      tem_erro = 1;
+    }
+
+    if (x < 1 || y < 1 || largura < 1 || altura < 1) {
+      tem_erro = 1;
+    }
+
+    // Texto maior que devia
+    if (strlen(texto) > 149) {
+      strcpy(texto_cortado, texto);
+      texto_cortado[149] = '\0';
+
+      /* Esse valor é um caso especifico em que o texto não é invalido porém ultrapassa
+      o limite do vetor declarado nesse programa */
+      tem_erro = 2;
+    }
+
+    // Texto inválido
+    for (int i = 0; i < strlen(texto); i++) {
+      if (!(texto[i] >= 32 && texto[i] <= 126)) {
+            tem_erro = 1;
+      }
+    }
+
+    if (tem_erro == 1) {
+      fprintf(erros, "%s %d %d %d %d %d %d %d %s\n", etiqueta, r, g, b, x, y, largura, altura, texto);
+    }
+    else {
+      if(!realoca_memoria(e, 1)) return -1;
+      strcpy(e->notas[e->n_notas - 1].etiqueta, etiqueta);
+      e->notas[e->n_notas - 1].cor.r = r;
+      e->notas[e->n_notas - 1].cor.g = g;
+      e->notas[e->n_notas - 1].cor.b = b;
+      e->notas[e->n_notas - 1].retangulo.x = x;
+      e->notas[e->n_notas - 1].retangulo.y = y;
+      e->notas[e->n_notas - 1].retangulo.largura = largura;
+      e->notas[e->n_notas - 1].retangulo.altura = altura;
+      if (tem_erro == 0) {
+        strcpy(e->notas[e->n_notas - 1].texto, texto);
+      }
+      else if (tem_erro == 2) {
+        fprintf(erros, "%s %d %d %d %d %d %d %d \"%s\"\n", etiqueta, r, g, b, x, y, largura, altura, texto);
+        strcpy(e->notas[e->n_notas - 1].texto, texto_cortado);
+      }
+    }
+  }
+  return 0;
+}
 
 int main (void) {
     // Arquivo de erros
@@ -829,12 +874,7 @@ int main (void) {
         return -1;
     }
 
-    estado_t estado = { principal, NULL, 1, 1, 1, NULL, {0}, 0, "", 150, 40, {0}};
-    estado.notas = malloc(sizeof(nota));
-    if (estado.notas == NULL) {
-      printf("Não foi possível alocar memória.\n");
-      return -1;
-    }
+    estado_t estado = { principal, NULL, 0, 1, 1, NULL, {0}, 0, "", 150, 40, {0}};
 
     int conseguiu_ler = ler_notas(arq, erros, &estado);
     if (conseguiu_ler == -1) {

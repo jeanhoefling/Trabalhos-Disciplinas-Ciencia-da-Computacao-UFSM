6f6ea7cb5ac441b4b2f4b210bc8659769aecd469
jean WSL PC <jeanalemao2006@gmail.com>
2026-06-16 20:01:03 -0300

    lab1_t1 versão final pronta

--- laboratório_de_programação_I/trabalho1/t1.c
+++ laboratório_de_programação_I/trabalho1/t1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define pi 3.1415926535
+#define PI 3.1415926535
 
 double potencia (double base, int expoente) {
     double res = 1;
@@ -20,7 +20,7 @@ double potencia (double base, int expoente) {
     }
 }
 
-long fatorial (long numero) {
+double fatorial (double numero) {
     // Um valor de retorno padrão caso receba um valor invalido (negativo)
     if (numero < 0) {
         return -1;
@@ -37,11 +37,16 @@ double seno (double angulo) {
     double termo = angulo;
     double total = 0;
     long i = 1;
-    while (termo >= potencia(10, -10) || termo <= -potencia(10, -10)) {
+
+    /*Aqui pensei numa solução que não precisava ficar recalculando fatorial e potência
+    toda vez, mas deixei assim porque pedia no enunciado para usar as funções anteriores*/
+    while (termo > potencia(10, -10) || termo < -potencia(10, -10)) {
         total += termo;
         i += 2;
         termo = potencia(-1, i/2)*(potencia(angulo, i)/fatorial(i));
     }
+    
+    total += termo;
     return total;
 }
 
@@ -51,7 +56,7 @@ double raiz (double numero) {
         return -1;
     }
 
-    double termo_anterior = numero/2 + 1, termo_atual = numero/2;
+    double termo_anterior = 5, termo_atual = 6;
     while (termo_atual - termo_anterior > potencia(10, -10) || termo_atual - termo_anterior < -potencia(10, -10)) {
         termo_anterior = termo_atual;
         termo_atual = (termo_anterior+numero/termo_anterior)/2;
@@ -69,7 +74,7 @@ double cosseno (double angulo) {
     } 
 
     double cosseno_angulo = raiz(1-potencia(seno_angulo, 2));
-    int quadrante = ((int)(angulo/(pi/2)))%4 + 1;
+    int quadrante = ((int)(angulo/(PI/2)))%4 + 1;
     if (quadrante == 2 || quadrante == 3) {
         cosseno_angulo = -cosseno_angulo;
     }
@@ -78,7 +83,7 @@ double cosseno (double angulo) {
 
 //Função criada para converter de angulo de radiano para grau
 double conversor (int valor) {
-    double valor_convertido_em_graus = ((double)valor/180)*pi;
+    double valor_convertido_em_graus = ((double)valor/180)*PI;
     return valor_convertido_em_graus;
 }
 
@@ -89,13 +94,13 @@ void tabela () {
     printf("\u251C\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2524\n");
 
     //Meio da tabela
-    for (int i = 0; i <= 349; i +=5) {
+    for (int i = 0; i <= 79; i +=5) {
         printf("\u2502%6i\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502\n", i, cosseno(conversor(i)), seno(conversor(i)), cosseno(conversor(i+1)), seno(conversor(i+1)), cosseno(conversor(i+2)), seno(conversor(i+2)), cosseno(conversor(i+3)), seno(conversor(i+3)), cosseno(conversor(i+4)), seno(conversor(i+4)));
         printf("\u251C\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2524\n");
     }
 
     //Inferior da tabela
-    printf("\u2502   355\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502\n", cosseno(conversor(355)), seno(conversor(355)), cosseno(conversor(356)), seno(conversor(356)), cosseno(conversor(357)), seno(conversor(357)), cosseno(conversor(358)), seno(conversor(358)), cosseno(conversor(359)), seno(conversor(359)));
+    printf("\u2502    85\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502%7.4f\u2502\n", cosseno(conversor(85)), seno(conversor(85)), cosseno(conversor(86)), seno(conversor(86)), cosseno(conversor(87)), seno(conversor(87)), cosseno(conversor(88)), seno(conversor(88)), cosseno(conversor(89)), seno(conversor(89)));
     printf("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2534\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518\n");
 
 }
